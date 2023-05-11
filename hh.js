8baa4a758e65faf827d8672ed4c4b65e097fc72a
Peter Dobbins <109819737+Peter-Dobbins@users.noreply.github.com>
2023-05-11 14:13:30 +0100

    Add files via upload

---
+++ hh.js
@@ -0,0 +1,9 @@
+
+document.getElementById("salary").onmouseover = function() {
+    document.getElementById("salary").classList.add("animate");
+
+}
+document.getElementById("salary").onmouseout = function() {
+    document.getElementById("salary").classList.remove("animate");
+
+}
